79d047106a22d39549e6da61b3f5d6af40e50dc6
Fynja <45318697+Fynja@users.noreply.github.com>
2020-08-07 21:18:12 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,20 +1,24 @@
 import socket
 import threading
-
-ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = input("Enter server IP: ")
-port = int(input("Enter server port: "))
-print('Attempting to connect')
-ClientSocket.connect((host, port))
-Response = ClientSocket.recv(1024)
-USRN = input("Username: ")
-
+###############################################################################
+#receives data from server and prints on screen
 def receive():
     while True:
         msg = ClientSocket.recv(1024)      
         print("\n", msg.decode("utf-8"))
+###############################################################################
+#input for host ip, port and user's username
+host = input("Enter server IP: ")
+port = int(input("Enter server port: "))
+USRN = input("Username: ")
+#setup connection to host 
+ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Attempting to connect')
+ClientSocket.connect((host, port))
+###############################################################################
+#receive thread to get messages without being blocked
 threading._start_new_thread(receive, ())
-
+#this loop gets user input and sends it to the server with the username appended
 while True:
     Input = input()
     msg = "{0} said: {1}".format(USRN,Input)

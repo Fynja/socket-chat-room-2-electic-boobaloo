f261eca0c0240c21ec20a2e6f805dca5f07375fa
lucianojamimbo <lucianojamimbo@gmail.com>
2020-08-08 15:37:04 +0100

    added code for receiving log list from server upon connection

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import pickle
 from encodedecode import encode
 from encodedecode import decode 
 ###############################################################################
@@ -22,6 +23,26 @@ print("Attempting to connect")
 ClientSocket.connect((host, port))
 print("Connected")
 ###############################################################################
+
+#receive files from server upon connection:
+#this code is for accepting the log file from the server, which is supplied on joining
+HEADERSIZE = 10
+full_msg = b''
+new_msg = True
+while True:
+    msg = ClientSocket.recv(16)
+    if new_msg:
+        msglen = int(msg[:HEADERSIZE])
+        new_msg = False 
+    full_msg += msg 
+    if len(full_msg)-HEADERSIZE == msglen:
+        logs = pickle.loads(full_msg[HEADERSIZE:])
+        break
+#print logs to terminal:
+for i in logs:
+    print(decode(key, i))
+
+#normal message send/receive:
 #receive thread to get messages without being blocked
 threading._start_new_thread(receive, (key, ))
 #this loop gets user input and sends it to the server with the username appended

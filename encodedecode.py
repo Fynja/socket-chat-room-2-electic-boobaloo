c8bb7c00c92a3589a69009d3436ee3321a56c2f8
lucianojamimbo <lucianojamimbo@gmail.com>
2020-08-08 14:17:57 +0100

    removed prints

--- encodedecode.py
+++ encodedecode.py
@@ -7,7 +7,6 @@ def encode(key, string):
             coded_ord = key_ord + str_i_ord
             encoded = encoded + str(chr(coded_ord))
     except:
-        print("ERROR: a message could not be encoded!")
         encoded = "ERROR_COULD_NOT_BE_ENCODED"
     return encoded
 
@@ -20,6 +19,5 @@ def decode(key, string):
             coded_ord = str_i_ord - key_ord
             decoded = decoded + str(chr(coded_ord))
     except:
-        print("ERROR: a message could not be decoded!")
         decoded = "ERROR_COULD_NOT_BE_DECODED"
     return decoded

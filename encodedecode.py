29099789af543f0316e2557c79f5ca78892a189d
lucianojamimbo <lucianojamimbo@gmail.com>
2020-08-08 11:32:20 +0100

    Added error handling

--- encodedecode.py
+++ encodedecode.py
@@ -1,16 +1,25 @@
 def encode(key, string):
     encoded = ""
-    for i in range(len(string)):
-        key_ord = ord(key[i % len(key)])
-        str_i_ord = ord(string[i])
-        coded_ord = key_ord + str_i_ord
-        encoded = encoded + str(chr(coded_ord))
+    try:
+        for i in range(len(string)):
+            key_ord = ord(key[i % len(key)])
+            str_i_ord = ord(string[i])
+            coded_ord = key_ord + str_i_ord
+            encoded = encoded + str(chr(coded_ord))
+    except:
+        print("ERROR: a message could not be encoded!")
+        encoded = "ERROR_COULD_NOT_BE_ENCODED"
     return encoded
+
 def decode(key, string):
     decoded = ""
-    for i in range(len(string)):
-        key_ord = ord(key[i % len(key)])
-        str_i_ord = ord(string[i])
-        coded_ord = str_i_ord - key_ord
-        decoded = decoded + str(chr(coded_ord))
+    try:
+        for i in range(len(string)):
+            key_ord = ord(key[i % len(key)])
+            str_i_ord = ord(string[i])
+            coded_ord = str_i_ord - key_ord
+            decoded = decoded + str(chr(coded_ord))
+    except:
+        print("ERROR: a message could not be decoded!")
+        decoded = "ERROR_COULD_NOT_BE_DECODED"
     return decoded

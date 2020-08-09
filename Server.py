68fe3ed8b933af3c48502b00bed23d1a551d1df0
lucianojamimbo <lucianojamimbo@gmail.com>
2020-08-09 10:16:52 +0100

    small changes

--- Server.py
+++ Server.py
@@ -5,7 +5,8 @@ import pickle
 ###############################################################################
 #when a new connection is established this function is run on a new thread
 #it waits for information to be received from a client then calls msg_all_clients to pass that information to other clients
-def threaded_client(connection, log):
+def threaded_client(connection):
+    global log
     #send log file to client upon connection
     file = pickle.dumps(log)
     HEADERSIZE = 10
@@ -32,25 +33,24 @@ def msg_all_clients(msg):
     while clientID < len(clients):
         try:
             clients[clientID].send(str.encode(msg))
+            clientID += 1
         except Exception as e:
             print("===================================")
-            print("Could not send data to clientID {0} it is likely they are no longer present on the network".format(clientID))
+            print("Could not send data to client {0}. It is likely they are no longer present on the network.".format(clientID))
             print("Exception Info: ", e)
             try:
-                print("Attempting to remove problematic client information")
+                print("Attempting to remove problematic client information...")
                 clients.pop(clientID)
-                print("Client information removed")
-                clientID -= 1
+                print("Client information removed.")
             except Exception as e:
                 print("Could not remove client information for the following reason:")
                 print(e)
             print("===================================")
-        clientID += 1
 ###############################################################################
-#saves message history to server.log
+#saves message history to server.log every x seconds
 def savelog(log):
     x = 10
-    print("Log will be saved to file every {0} seconds".format(x))
+    print("Log will be saved to file every {0} seconds.".format(x))
     while True:
         with open("Server.log", 'wb') as f:
             pickle.dump(log, f)
@@ -65,25 +65,23 @@ try:
     with open('Server.log','rb') as f:
         log = pickle.load(f)
 except:
-    print("Log file not found, a new one will be created")
+    print("Log file not found, a new one will be created.")
     log = []
 threading._start_new_thread(savelog, (log, ))
 #listen for a connection:
-print("Waiting for a Connection..")
+print("Waiting for a Connection...")
 ServerSocket.listen(5)
 #define some variables
 clients = []
-ThreadCount = 0
 while True:
     #accept new connection and create thread for new client:
     Client, address = ServerSocket.accept()
-    threading._start_new_thread(threaded_client, (Client, log))
+    threading._start_new_thread(threaded_client, (Client, ))
     #adjust variables:
     clients.append(Client)
-    ThreadCount += 1
     #print information to console:
+    print("===================================")
     print("New connection from: {0} : {1}".format(address[0], address[1]))
-    print("Thread Number: {0}".format(ThreadCount))
     print("Clients:")
     for c in clients:
         print(c)
